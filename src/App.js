313f1a64cf65ce89658c9f33f6df9369eac07183
olzhas828 <olzhas828@gmail.com>
2021-03-10 21:12:53 -0500

    fetch upd

--- src/App.js
+++ src/App.js
@@ -1,35 +1,68 @@
 import './App.css';
-import { useState } from 'react'
-const URL = 'https://www.metaweather.com/api'
-const fetchWeather = async (city) => {
-  const headers = {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
+import { useEffect, useState } from 'react'
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
   }
-  const response = await fetch(`${URL}/location/search/?query=${city}`, { headers })
-  const data = await response.json();
-  return data
+  return response;
+}
+const fetchLocationIDs = (location) => {
+  return new Promise((resolve, reject) => {
+    fetch(`/location/search/?query=${location}`)
+      .then(handleErrors)
+      .then(response => resolve(response.json()))
+      .catch(err => reject(err.statusText))
+  })
 
 }
+const fetchCityForecast = (woeid) => {
+  return new Promise((resolve, reject) => {
+    fetch(`https://weather-api-middle.herokuapp.com/2122265/${woeid}`, {})
+      .then(handleErrors)
+      .then(response => resolve(response.json()))
+      .catch(err => reject(err.statusText))
+  })
+}
+
 
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('')
+  const [foundCities, setFoundCities] = useState([])
+  const [forecastList, setForecastList] = useState([])
+  const [err, setErr] = useState()
   const handleTermChange = (e) => {
     let value = e.target.value
     setSearchTerm(value)
   }
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(fetchWeather(searchTerm))
-
+    fetchLocationIDs(searchTerm)
+      .then(data => setFoundCities(data))
+      .catch(err => setErr(err))
   }
+  useEffect(() => {
+    let promises = []
+    foundCities.forEach(city => promises.push(fetchCityForecast(city.woeid)))
+    Promise.all(promises)
+      .then(data => { console.log(data); setForecastList(data) })
+      .catch(err => setErr(err))
+  }, [foundCities])
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <input type='text' placeholder='Enter city name' onChange={handleTermChange} />
         <input type='submit' value='Search' />
       </form>
+      <div>
+        <ul>
+          {forecastList.map((city, i) =>
+            <li key={i}>
+              {city.title}
+            </li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
